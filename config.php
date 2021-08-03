2388db11070f979d8f1357232686889095706099
Rakesh <rakesh.mentormerlin@gmail.com>
2021-08-03 10:29:33 +0530

    for teset commit

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 $sqlConnect = new mysqli("localhost","root","","test");
 
+
 if ($sqlConnect -> connect_errno) {
     echo "Failed to connect to MySQL: " . $sqlConnect -> connect_error;
     exit();
