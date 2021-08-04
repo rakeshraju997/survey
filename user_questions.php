0460f597aa703f677815c7ff61cb86e032c5a820
Rakesh <rakesh.mentormerlin@gmail.com>
2021-08-04 17:53:37 +0530

    phone view correction

--- user_questions.php
+++ user_questions.php
@@ -1,3 +1,19 @@
+<style>
+    .form-input{
+        margin: -12px 6px 0px;
+        position: absolute;   
+     }
+        .form-inputs{
+        margin: 16px 15px 0px;
+        position: absolute;   
+     }
+     .close-qstn-field{
+        position: relative;
+    top: 2.5em;
+    margin-left: 12em;
+     }
+        
+</style>
 <!doctype html>
 <html>
 <head>
@@ -16,40 +32,32 @@
             <div class="lg:w-1/2 md:w-2/3 mx-auto">
 <form class="" id="user_questions" method="POST">
                 <div class="question-form flex flex-wrap -m-2">
-                    <h2 class="font-medium title-font text-gray-900">Question 1</h2>
+                    <h2 class="font-medium title-font text-gray-900 form-input">Question 1</h2>
                     <div class="p-2 w-full">
                         <div class="relative">
                             <label for="message" class="leading-7 text-sm text-gray-600">Enter your question here.</label>
                             <textarea id="message" name="question" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                         </div>
                     </div>
-                    <div class="p-2 w-1/2">
-                        <div class="relative">
+                    <div class="w3-quarter w3-container p-2 ">
                             <label for="name" class="leading-7 text-sm text-gray-600">Option 1</label>
-                            <input type="text" id="name" name="A" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                        </div>
+                            <input type="text" id="name" name="A" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"><br>
                     </div>
-                    <div class="p-2 w-1/2">
-                        <div class="relative">
+                    <div class="w3-quarter w3-container p-2 ">
                             <label for="name" class="leading-7 text-sm text-gray-600">Option 2</label>
                             <input type="text" id="name" name="B" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                        </div>
                     </div>
-                    <div class="p-2 w-1/2">
-                        <div class="relative">
+                        <div class="w3-quarter w3-container p-2 ">
                             <label for="name" class="leading-7 text-sm text-gray-600">Option 3</label>
                             <input type="text" id="name" name="C" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                         </div>
-                    </div>
-                    <div class="p-2 w-1/2">
-                        <div class="relative">
+                        <div class="w3-quarter w3-container p-2">
                             <label for="name" class="leading-7 text-sm text-gray-600">Option 4</label>
                             <input type="text" id="name" name="D" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                         </div>
-                    </div>
                 </div>
                 <div class="p-2 w-full">
-                    <button class="add-question-button flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-sm">Add more</button>
+                    <button class="add-question-button flex  text-white bg-indigo-500  border-0 py-2 px-8 focus:outline-none rounded text-sm">Add more</button>
                 </div>
                 <div class="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
                     <!-- content -->
@@ -57,7 +65,7 @@
             </div>
             <div class="lg:w-1/2 md:w-2/3 mx-auto">
                 <div class="p-2 w-full">
-                    <button type="submit" class="flex text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded">Submit</button>
+                    <button type="submit" class="flex text-white mx-auto bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-500 rounded">Submit</button>
                 </div>
             </div>
 </form>
@@ -76,7 +84,7 @@
                 if (x < max_fields) { //max input box allowed
                     x++; //text box increment
                    // alert(x);
-                    $(wrapper).append('<div class="question-form flex flex-wrap -m-2"><h2 class="font-medium title-font text-gray-900">Question '+ x +'</h2><button type="button" class="close close_qstn remove_field" aria-label="Close"><span aria-hidden="true">&times;</span></button><div class="p-2 w-full"><div class="relative"><label for="message" class="leading-7 text-sm text-gray-600">Enter your question here.</label><textarea id="message" name="question" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea></div></div><div class="p-2 w-1/2"><div class="relative"><label for="name" class="leading-7 text-sm text-gray-600">Option 1</label><input type="text" id="name" name="A" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></div></div><div class="p-2 w-1/2"><div class="relative"><label for="name" class="leading-7 text-sm text-gray-600">Option 2</label><input type="text" id="name" name="B" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></div></div><div class="p-2 w-1/2"><div class="relative"><label for="name" class="leading-7 text-sm text-gray-600">Option 3</label><input type="text" id="name" name="C" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></div></div><div class="p-2 w-1/2"><div class="relative"><label for="name" class="leading-7 text-sm text-gray-600">Option 4</label><input type="text" id="name" name="D" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></div></div></div><br></div>'); //add input box
+                    $(wrapper).append('<div class="question-form flex flex-wrap -m-2"><h2 class="font-medium title-font text-gray-900 form-inputs">Question '+ x +'</h2><button type="button" class="close close_qstn remove_field close-qstn-field" aria-label="Close"><span aria-hidden="true">&times;</span></button><div class="p-2 w-full "><div class="w3-quarter w3-container p-2 "><label for="message" class="leading-7 text-sm text-gray-600">Enter your question here.</label><textarea id="message" name="question" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea></div></div><div class="w3-quarter w3-container p-2 "><label for="name" class="leading-7 text-sm text-gray-600">Option 1</label><input type="text" id="name" name="A" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></div><div class="w3-quarter w3-container p-2 "><label for="name" class="leading-7 text-sm text-gray-600">Option 2</label><input type="text" id="name" name="B" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></div><div class="w3-quarter w3-container p-2 "><label for="name" class="leading-7 text-sm text-gray-600">Option 3</label><input type="text" id="name" name="C" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></div><div class="w3-quarter w3-container p-2 "><label for="name" class="leading-7 text-sm text-gray-600">Option 4</label><input type="text" id="name" name="D" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></div></div><br></div>'); //add input box
                 }
             });
             $(wrapper).on("click", ".remove_field", function(e) { //user click on remove text
