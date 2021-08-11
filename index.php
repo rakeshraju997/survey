e07b3ba33b86b51c731e380d3b90b09d89c72315
Rakesh <rakesh.mentormerlin@gmail.com>
2021-08-11 11:06:57 +0530

    index msg page

---
+++ index.php
@@ -0,0 +1,104 @@
+<?php include 'header.php'?>
+<section class="text-gray-600 body-font overflow-hidden">
+    <div class="container px-5 py-3 mx-auto">
+        <div class="lg:w-4/5 mx-auto flex flex-wrap">
+            <div class="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+                <h2 class="text-sm title-font text-gray-500 tracking-widest">Mentor Merlin</h2>
+                <h1 class="text-gray-900 text-3xl title-font font-medium mb-4">Welcomes you to NMC CBT study material
+                    feedback forum</h1>
+
+
+                <div class=" md:pr-10 md:py-6">
+                    <div class="flex relative pb-2">
+                        <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
+                            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                        </div>
+                        <div
+                            class="flex-shrink-0 w-10 h-10 rounded-full bg-blue-500 inline-flex items-center justify-center text-white relative z-10">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+                            </svg>
+                        </div>
+                        <div class="flex-grow pl-4">
+                            <!-- <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 1</h2> -->
+                            <p class="leading-relaxed">Identify the relevancy of current materials.</p>
+                        </div>
+                    </div>
+                    <div class="flex relative pb-2">
+                        <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
+                            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                        </div>
+                        <div
+                            class="flex-shrink-0 w-10 h-10 rounded-full bg-blue-500 inline-flex items-center justify-center text-white relative z-10">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                            </svg>
+                        </div>
+                        <div class="flex-grow pl-4">
+                            <!-- <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 2</h2> -->
+                            <p class="leading-relaxed">Identify the relevancy of current questions.</p>
+                        </div>
+                    </div>
+                    <div class="flex relative pb-4">
+                        <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
+                            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                        </div>
+                        <div class="flex-shrink-0 w-10 h-10 rounded-full bg-blue-500 inline-flex items-center justify-center text-white relative z-10">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                <circle cx="12" cy="5" r="3"></circle>
+                                <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
+                            </svg>
+                        </div>
+                        <div class="flex-grow pl-4">
+                            <!-- <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 3</h2> -->
+                            <p class="leading-relaxed">Add additional relevant information.</p>
+                        </div>
+                    </div>
+                    <div class="flex relative">
+                        <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
+                            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                        </div>
+                        <div class="flex-shrink-0 w-10 h-10 rounded-full bg-blue-500 inline-flex items-center justify-center text-white relative z-10">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                                <circle cx="12" cy="7" r="4"></circle>
+                            </svg>
+                        </div>
+                        <div class="flex-grow pl-4">
+                            <!-- <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 4</h2> -->
+                            <p class="leading-relaxed">There is no time limit.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-50 object-cover object-center rounded"
+                src="http://localhost/img/nurse.png">
+        </div>
+        <div class="flex flex-col pt-4 text-center w-full mb-20">
+            <!-- <h1 class="text-2xl font-medium title-font mb-4 text-gray-900">OUR TEAM</h1> -->
+            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Your effort for supporting others will be rewarded in
+                Mentor Merlin’s voucher which you can use for future purchases. The validity of the voucher will be one
+                year.</p>
+
+            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+                The valuation of your reward will be decided within 5 working days by comparing the relevancy and the
+                accuracy of the work you have completed.</p>
+
+            <div class="flex-left">
+                <a href="http://localhost/img/user_page.php" class="flex-left ml-auto text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded">Next</a>
+                <!-- <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        class="w-5 h-5" viewBox="0 0 24 24">
+                        <path
+                            d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z">
+                        </path>
+                    </svg>
+                </button> -->
+            </div>
+        </div>
+    </div>
+</section>
